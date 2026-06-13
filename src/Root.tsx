ff9c9c77ec59889fdb593c05555ac3837c0b63b4
messaynew-cyber <messaynew@gmail.com>
2026-06-13 18:03:39 +0300

    Wire up EthiopiaDemo composition

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,8 +1,11 @@
+import React from "react";
 import { Composition } from "remotion";
+import { registerRoot } from "remotion";
 import { AdwaDemo } from "./AdwaDemo";
 import { TobiaPromo } from "./TobiaPromo";
+import { EthiopiaDemo } from "./EthiopiaDemo";
 
-export const RemotionRoot = () => {
+export const Root: React.FC = () => {
   return (
     <>
       <Composition
@@ -21,6 +24,16 @@ export const RemotionRoot = () => {
         width={1920}
         height={1080}
       />
+      <Composition
+        id="EthiopiaDemo"
+        component={EthiopiaDemo}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
     </>
   );
 };
+
+registerRoot(Root);
