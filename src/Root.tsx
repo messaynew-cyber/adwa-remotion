6f07e1f3d5eb54c4b5541019e19ea3e77c58af0d
messaynew-cyber <messaynew@gmail.com>
2026-06-19 23:46:31 +0300

    feat: register AfricaPromo composition in Root.tsx

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,6 +3,7 @@ import { Composition } from "remotion";
 import { AdwaDemo } from "./AdwaDemo";
 import { TobiaPromo } from "./TobiaPromo";
 import { EthiopiaDemo } from "./EthiopiaDemo";
+import { AfricaPromo } from "./AfricaPromo";
 
 export const Root: React.FC = () => {
   return (
@@ -26,6 +27,14 @@ export const Root: React.FC = () => {
       <Composition
         id="EthiopiaDemo"
         component={EthiopiaDemo}
+        durationInFrames={900}
+        fps={30}
+        width={1920}
+        height={1080}
+      />
+      <Composition
+        id="AfricaPromo"
+        component={AfricaPromo}
         durationInFrames={900}
         fps={30}
         width={1920}
